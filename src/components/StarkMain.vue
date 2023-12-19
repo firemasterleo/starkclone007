0b479238a37ab1b98517d38f5b18222d28a6f9fd
firemasterleo <firemasterleo@outlook.com>
2023-12-19 19:54:04 +0000

    version 3

--- src/components/StarkMain.vue
+++ src/components/StarkMain.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="grandparent">
+
 
 
 <div class="parentcontainer">
@@ -12,20 +12,50 @@
                 </div>
                 <div class="navlist">
                     <ul class="nav">
+
+                        <div class="product" @mouseover="showproductpopup" @mouseleave="hideproductpopup">
+
                         <li>
                             <a href="">PRODUCT</a>
                             <svg xmlns='http://www.w3.org/2000/svg'  viewBox='0 0 24 24' fill='#000000' width='24' height='24'><path d="M11.178 19.569a.998.998 0 0 0 1.644 0l9-13A.999.999 0 0 0 21 5H3a1.002 1.002 0 0 0-.822 1.569l9 13z"></path></svg>
                         </li>
+
+                        <div class="productpopup" v-if="is_producthovered">
+                            <h1>helloworld</h1>
+                        </div>
+                        </div>
+
+
+
+                        <div class="pricing" >
+
                         <li>
                             <a href="">PRICING</a>
                         </li>
+                        </div>
+
+
+
+                        <div class="support" >
+
                         <li>
                             <a href="">SUPPORT</a>
                         </li>
+                        </div>
+
+
+
+                        <div class="resources" @mouseover="showresourcespopup" @mouseleave="hideresourcespopup" >
+
                         <li>
                             <a href="">RESOURCES</a>
                             <svg xmlns='http://www.w3.org/2000/svg'  viewBox='0 0 24 24' fill='#000000' width='24' height='24'><path d="M11.178 19.569a.998.998 0 0 0 1.644 0l9-13A.999.999 0 0 0 21 5H3a1.002 1.002 0 0 0-.822 1.569l9 13z"></path></svg>
                         </li>
+                        <div class="resourcespopup" v-if="is_resourceshovered">
+                            <h1>helloworld</h1>
+                        </div>
+                        </div>
+
                     </ul>
                 </div>
             </div>
@@ -45,114 +75,235 @@
             </div>
             <div class="headerright">
                 <button class="startfree">START FREE TRIAL</button>
-                <div class="hamburger" @click="ToggleMenu">
-                <svg xmlns='http://www.w3.org/2000/svg'  viewBox='0 0 24 24' fill='#000000' width='28' height='28'><path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path></svg>
-
+                <div class="hamburger hamburger--spin-r" :class="{ 'is-active': is_expanded }" @click="ToggleMenu">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
                 </div>
             </div>
         </div>
     </div>
     <div class="menuslidelist" :class="{ 'is-expanded': is_expanded }">
-        <button class="login"></button>
+        <button class="login">LOG IN</button>
         <div class="products">
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-            <a href="">PRODUCTS</a>
-           
-            <div class="productlist"></div>
+            <a href="">PRODUCT</a>        
+            <div class="productlist">
+                <a href="">Stark Web App</a>
+                <a href="">Stark for Figma</a>
+                <a href="">Stark for FigJam</a>
+                <a href="">Stark for Sketch</a>
+                <a href="">Stark for Adobe XD</a>
+                <a href="">Stark for Chrome</a>
+                <a href="">Stark for Firefox</a>
+                <a href="">Stark for Edge</a>
+                <a href="">Stark for Safari</a>
+                <a href="">Stark for Arc</a>
+                <a href="">Stark for Brave</a>
+            </div>
         </div>
-        <div class="pricing">
-            <a href="PRICING"></a>
+        <div class="menuitem">
+            <a href="">PRICING</a>
         </div>
+        <div class="menuitem">
+            <a href="">SUPPORT</a>
+        </div>
+        <div class="menuitem">
+            <a href="">BLOG</a>
+        </div>
+        <div class="menuitem">
+            <a href="">LIBRARY</a>
+        </div>
+        <div class="menuitem">
+            <a href="">COMMUNITY</a>
+        </div>
+
     </div>
     <div class="body" v-if="!is_expanded">
-        <h1>hellvgggvo</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
-        <h1>helloplase</h1>
+        <div class="leftpanel">
+            <h3>Search Library</h3>
 
+            <div class="searchbar">
+                <div class="searchicon">
+                <svg xmlns='http://www.w3.org/2000/svg'  viewBox='0 0 24 24' fill='#000000' width='20' height='20'><path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path></svg>
+
+                </div>
+            <input type="text" placeholder="input">
+            </div>
+            <div class="menulist">
+                <ul>
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">Collections</a>
+                    </li>
+                    <li class="categories">
+                        <a href="">Categories</a>
+
+
+                    </li>
+                </ul>
+            </div>
+            <div class="contribute">
+                <h3>Contribute to Stark</h3>
+                <p>Got any accessibility resources or news ?</p>
+                <button>Submit a Resource</button>
+            </div>
+        </div>
+
+        <div class="rightpanel">
+        <div class="section1">
+            <div class="new">
+
+                <div>
+                    <h1>New in the Library</h1>
+                    <a href="">VIEW ALL</a>
+                </div>
+
+                <p>Newly added accessibility resources,guides,and more.</p>
+            </div>
+        </div>
+        <div class="section2">
+            <div class="box">
+                <div class="logo">
+                    <img src="../assets/starklogo2.png" alt="">
+                </div>
+                <h1>RESOURCES</h1>
+                <h2>Making Accessible Books</h2>
+                
+            </div>
+            <div class="box">
+                                <div class="logo">
+                    <img src="../assets/starklogo2.png" alt="">
+                </div>
+                <h1>RESOURCES</h1>
+                <h2>Making Accessible Books</h2>
+                
+            </div>
+            <div class="box">
+                                <div class="logo">
+                    <img src="../assets/starklogo2.png" alt="">
+                </div>
+                <h1>RESOURCES</h1>
+                <h2>Making Accessible Books</h2>
+            </div>
+            <div class="box">
+                                <div class="logo">
+                    <img src="../assets/starklogo2.png" alt="">
+                </div>
+                <h1>RESOURCES</h1>
+                <h2>Making Accessible Books</h2>
+            </div>
+            <div class="box">
+                                <div class="logo">
+                    <img src="../assets/starklogo2.png" alt="">
+                </div>
+                <h1>RESOURCES</h1>
+                <h2>Making Accessible Books</h2>
+            </div>
+            <div class="box">
+                                <div class="logo">
+                    <img src="../assets/starklogo2.png" alt="">
+                </div>
+                <h1>RESOURCES</h1>
+                <h2>Making Accessible Books</h2>
+            </div>
+        </div>
+
+        </div>
+    </div>
+    <div class="mobilebody" v-if="!is_expanded">
+        <div class="menulistparent">
+            <div class="menulist">
+                <ul class="navlist">
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">Collections</a>
+                    </li>
+                    <li class="categories">
+                        <a href="">Categories</a>
+
+
+                    </li>
+                </ul>
+            <div class="searchbar">
+                <div class="searchicon">
+                <svg xmlns='http://www.w3.org/2000/svg'  viewBox='0 0 24 24' fill='#000000' width='20' height='20'><path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path></svg>
+
+                </div>
+            <input type="text" placeholder="input">
+            </div>
+            </div>
+
+        </div>
+<!-- 
+        <div class="sections">
+        <div class="section1">
+            <div class="new">
+
+                <div>
+                    <h1>New in the Library</h1>
+                    <a href="">VIEW ALL</a>
+                </div>
+
+                <p>Newly added accessibility resources,guides,and more.</p>
+            </div>
+        </div>
+        <div class="section2">
+            <div class="box">
+                <div class="logo">
+                    <img src="../assets/starklogo2.png" alt="">
+                </div>
+                <h1>RESOURCES</h1>
+                <h2>Making Accessible Books</h2>
+                
+            </div>
+            <div class="box">
+                                <div class="logo">
+                    <img src="../assets/starklogo2.png" alt="">
+                </div>
+                <h1>RESOURCES</h1>
+                <h2>Making Accessible Books</h2>
+                
+            </div>
+            <div class="box">
+                                <div class="logo">
+                    <img src="../assets/starklogo2.png" alt="">
+                </div>
+                <h1>RESOURCES</h1>
+                <h2>Making Accessible Books</h2>
+            </div>
+            <div class="box">
+                                <div class="logo">
+                    <img src="../assets/starklogo2.png" alt="">
+                </div>
+                <h1>RESOURCES</h1>
+                <h2>Making Accessible Books</h2>
+            </div>
+            <div class="box">
+                                <div class="logo">
+                    <img src="../assets/starklogo2.png" alt="">
+                </div>
+                <h1>RESOURCES</h1>
+                <h2>Making Accessible Books</h2>
+            </div>
+            <div class="box">
+                                <div class="logo">
+                    <img src="../assets/starklogo2.png" alt="">
+                </div>
+                <h1>RESOURCES</h1>
+                <h2>Making Accessible Books</h2>
+            </div>
+        </div>
+
+        </div> -->
     </div>
     <div class="footer"></div>
 
 </div>
-</div>
+
 
 <!-- <h1>
 okay so to get this 
@@ -187,6 +338,28 @@ const is_expanded = ref(false)
 const ToggleMenu = () => {
     is_expanded.value = !is_expanded.value;
 }
+
+
+
+const is_producthovered = ref(false)
+
+const  showproductpopup= () => {
+    is_producthovered.value = true;
+}
+const  hideproductpopup= () => {
+    is_producthovered.value = false;
+}
+
+
+const is_resourceshovered = ref(false)
+
+const  showresourcespopup= () => {
+    is_resourceshovered.value = true;
+}
+const  hideresourcespopup= () => {
+    is_resourceshovered.value = false;
+}
+
 
 </script>
 
